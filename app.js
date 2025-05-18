a8ac9f8051c8aedccd676541dd571f1ab893d0b6
everl2001 <everillesca19@gmail.com>
2025-05-17 21:34:11 -0300

    Deploy API to Railway

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express') // require -> commonJS
+const crypto = require('node:crypto')
+const cors = require('cors')
+
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schema/movies')
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'http://movies.com',
+            'http://midu.dev',
+        ]
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+
+// Todos los recursos que sean Movies se identifican con /movies
+app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessanle Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+    // en base de datos
+    const newMovie = {
+        id: crypto.randomUUID(),
+        ...result.data
+    }
+    // Esto no seria REST, pq estamos guardando
+    // el estado de la app en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie) // actualizar el cache del cliente
+})
+
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    movies[movieIndex] = updateMovie
+
+    return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})
